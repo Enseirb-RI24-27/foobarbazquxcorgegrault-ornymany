20b4eed75d8b4be7bf9e90c587247f64d1133d0e
abaril <abaril@bordeaux-inp.fr>
2026-01-12 16:59:16 +0100

    init

---
+++ src/main/java/com/jad/Foo.java
@@ -0,0 +1,12 @@
+package com.jad;
+
+
+import java.util.List;
+
+public class Foo {
+    private Bar bar;
+    private List<Baz> bazs;
+    private Qux qux;
+    private Corge corge;
+    private List<Grault> graults;
+}
